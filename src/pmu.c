7694917b459f67520398e55e25cda2afd1a48f6b
Anand Patel <anandpatel1221178@gmail.com>
2026-05-12 10:40:22 +0200

    PMU done

--- src/pmu.c
+++ src/pmu.c
@@ -59,25 +59,70 @@ int pmu_init(pmu_t *pmu, uint32_t num_workers)
     return 0; /* Success for the init */
 }
 
-static
+/* Sampler thread -> background loop kthat reads counters every 10ms */
+static void *pmu_sampler_thread(void *arg)
+{
+    /* cast from void to pmu */
+    pmu_t *pmu = (pmu_t *)arg;
+
+    while(pmu->keep_running == 1)
+    {
+        /* For each worker */
+        for (int i = 0; i < pmu->num_workers; ++i)
+        {
+            uint64_t curr_count; /* var to store the fd reads */
+            uint64_t delta; /* change in reads */
+            read(pmu->fds[i], &curr_count, sizeof(curr_count));
+            delta = curr_count - pmu->previous_readings[i];
+            /* We divide by the interval to get the RATE - cache misses per millisecond for worker i*/
+            pmu->miss_rates[i] = (float)delta /10.0f;
+            pmu->previous_readings[i] = curr_count;
+        }
+        usleep(10000);
+    }
+
+    return NULL;
+}
 
 void pmu_start(pmu_t *pmu)
 {
-    /* Add creation of pthread */
+    /* Tell thread it is allowed to run*/
     pmu->keep_running = 1;
+    /* Reset and enable all h/s counters */
     for (int i = 0; i < pmu->num_workers; i++)
     {
             ioctl(pmu->fds[i], PERF_EVENT_IOC_RESET, 0);
             ioctl(pmu->fds[i], PERF_EVENT_IOC_ENABLE, 0);
     }
+
+    /* aluanch background thread */
+    pthread_create(&pmu->sampler_thread, NULL, pmu_sampler_thread, pmu);
+    printf("[TOPOSTEAL] PMU Sampler Thread started.\n");
 }
 
 void pmu_stop(pmu_t *pmu)
 {
     /* add destruction of pthread */
     pmu->keep_running = 0;
+
+    /* Wait for thread to safely inish last read and exit */
+    pthread_join(pmu->sampler_thread, NULL);
+
     for (int i = 0; i < pmu->num_workers; i++)
     {
             ioctl(pmu->fds[i], PERF_EVENT_IOC_DISABLE, 0);
+    }
+    printf("[TOPOSTEAL] PMU Sampler Thread stopped.\n");
+}
+
+float pmu_get_miss_rate(pmu_t *pmu, uint32_t worker_id)
+{
+    /* BOunds check! */
+    if(worker_id < pmu->num_workers)
+    {
+        return pmu->miss_rates[worker_id];
+    }
+    else{
+        return -1;
     }
 }
